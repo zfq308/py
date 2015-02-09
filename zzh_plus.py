ded3104586b66fd34bc295f01dd1deffaee82a64
spruce <spruce.ks@gmail.com>
2015-02-09 18:30:45 +0800

    temp

--- zzh_plus.py
+++ zzh_plus.py
@@ -3,11 +3,19 @@ import thread
 import time
 import shutil
 import time
+import random
 
-rootPath = 'E:/Newfolder2'
-logFile = 'E:/log.txt'
-errorFile = 'E:/error.txt'
-resultPath = 'E:/result_new'
+# rootPath = 'E:/Newfolder2'
+# logFile = 'E:/log.txt'
+# errorFile = 'E:/error.txt'
+# resultPath = 'E:/result_new'
+# allFile = 'E:/all_file.txt'
+
+rootPath = '/Users/spruce/Downloads/src'
+logFile = '/Users/spruce/Downloads/log.txt'
+errorFile = '/Users/spruce/Downloads/error.txt'
+resultPath = '/Users/spruce/Downloads/result_new'
+allFile = '/Users/spruce/Downloads/all_file_online.txt'
 
 files = []
 filesName = []
@@ -16,22 +24,50 @@ counter = 0
 def tree(path):
     if os.path.isfile(path):
         files.append(path)
-        filesName.append(int(os.path.basename(files).split('.')[0].split('_')[1]))
+        filesName.append(int(os.path.basename(path).split('.')[0].split('_')[1]))
     else:
         for temp in os.listdir(path):
             tree(path + os.sep + temp)
 
 def sort():
     log('Start sort...')
-    for i in range(len(files)):
-        filei = os.path.basename(files[i]).split('.')[0].split('_')[1]
-        for j in range(i+1, len(files)):
-            filej = os.path.basename(files[j]).split('.')[0].split('_')[1]
+    for i in range(len(filesName)):
+        filei = filesName[i]
+        for j in range(i+1, len(filesName)):
+            filej = filesName[j]
             if filej < filei:
+                filesName[i], filesName[j] = filesName[j], filesName[i]
                 files[i], files[j] = files[j], files[i]
     log('Sort done...')
-    
+
+def insertion_sort():
+    log('Start insertion_sort...')
+    total = len(filesName)
+    if len(filesName) == 1:
+        return
+    for i in xrange(1, len(filesName)):
+        if i % 1000 == 0:
+            log('Sort : ' + str(i) + '/' + str(total))
+        temp = filesName[i]
+        tempFile = files[i]
+        j = i - 1
+        while j >= 0 and temp < filesName[j]:
+            filesName[j + 1] = filesName[j]
+            files[j + 1] = files[j]
+            j -= 1
+        filesName[j + 1] = temp
+        files[j + 1] = tempFile
+    log('Sort done...')
+
+def saveFileName():
+    f = '/Users/spruce/Downloads/all_file_after_sort.txt'
+    log = open(f, 'a')
+    for file in files:
+        log.write(file + '\r')
+    log.close()
+
 def log(msg):
+    msg = time.ctime() + '\t' + msg + '\r'
     print msg
     log = open(logFile, 'a')
     log.write(time.ctime() + '\t' + msg + '\r')
@@ -85,14 +121,27 @@ def do(file):
     end = time.time()
     msg =  str(counter) + '/' + str(len(files)) + ',' + file + ',' + str(end - before)
     log(msg)
-    
+
+def load():
+    print 'load ' + allFile
+    f = open(allFile, 'r')
+    for l in f:
+        for path in l.split('\r'):
+            if path == '' or path == '\r':
+                continue
+            print path
+            files.append(path)
+            filesName.append(int(os.path.basename(path).split('.')[0].split('_')[1]))
+    f.close()
+
 def main():
     log('Start...')
     # get all file, save to files
-    tree(rootPath)
+    # tree(rootPath)
+    load()
     log('Total file number : ' + str(len(files)))
     # sort files
-    sort()
+    insertion_sort()
     for file in files:
         log(file)
     if os.path.exists(resultPath):
@@ -102,4 +151,10 @@ def main():
         do(file)
 
 if __name__ == '__main__':
-    main()
+    #main()
+    load()
+    insertion_sort()
+    saveFileName()
+    
+
+
