5442669158e074a89ddeaddb9966a2a463ff7c21
spruce <spruce.ks@gmail.com>
2015-02-12 18:02:18 +0800

    temp

--- zzh_plus.py
+++ zzh_plus.py
@@ -1,27 +1,38 @@
 import os
-import thread
 import time
 import shutil
 import time
-import random
 import sys
 
-DATA_SRC = 'f:/Share/Newfolder2/'
-LOG_FILE = 'f:/zzh/log.txt'
-ERROR_FILE = 'f:/zzh/error.txt'
-RESULT_PATH = 'f:/zzh/result_new'
-UNSORT_FILE_NAMES = 'f:/zzh/all_file_online.txt'
-SORTED_FILE_NAMES = 'f:/zzh/all_file_after_sort.txt'
+# 原始数据存放的目录
+DATA_SRC = 'e:/Newfolder2/'
+# 日志
+LOG_FILE = 'e:/log.txt'
+# 异常数据存放的路径
+ERROR_FILE = 'e:/error.txt'
+# 处理后的结果存放的路径
+RESULT_PATH = 'e:/result_dist/result'
+# 未排序的文件名
+UNSORT_FILE_NAMES = 'e:/all_file_online.txt'
+# 排序后的文件名
+SORTED_FILE_NAMES = 'e:/all_file_after_sort.txt'
 
-# DATA_SRC = '/Users/spruce/Downloads/src'
-# LOG_FILE = '/Users/spruce/Downloads/log.txt'
-# ERROR_FILE = '/Users/spruce/Downloads/error.txt'
-# RESULT_PATH = '/Users/spruce/Downloads/result_new'
-# UNSORT_FILE_NAMES = '/Users/spruce/Desktop/zzh/all_file_online.txt'
-# SORTED_FILE_NAMES = '/Users/spruce/Downloads/all_file_after_sort.txt'
+# # 原始数据存放的目录
+# DATA_SRC = 'e:/Newfolder2/'
+# # 日志
+# LOG_FILE = 'e:/log.txt'
+# # 异常数据存放的路径
+# ERROR_FILE = 'e:/error.txt'
+# # 处理后的结果存放的路径
+# RESULT_PATH = 'e:/result_dist/result'
+# # 未排序的文件名
+# UNSORT_FILE_NAMES = 'e:/all_file_online.txt'
+# # 排序后的文件名
+# SORTED_FILE_NAMES = 'e:/all_file_after_sort.txt'
 
 files = []
 filesName = []
+
 counter = 0
 
 temp = {}
@@ -34,6 +45,7 @@ def tree(path):
         for temp in os.listdir(path):
             tree(path + os.sep + temp)
 
+
 def sort():
     log('Start sort...')
     for i in range(len(filesName)):
@@ -44,6 +56,7 @@ def sort():
                 filesName[i], filesName[j] = filesName[j], filesName[i]
                 files[i], files[j] = files[j], files[i]
     log('Sort done...')
+
 
 def insertion_sort():
     log('Start insertion_sort...')
@@ -64,11 +77,13 @@ def insertion_sort():
         files[j + 1] = tempFile
     log('Sort done...')
 
-def saveFileName():
-    log = open(SORTED_FILE_NAMES, 'a')
+
+def saveFileName(fileName):
+    log = open(fileName, 'a')
     for file in files:
         log.write(file + '\r')
     log.close()
+
 
 def log(msg):
     msg = time.ctime() + '\t' + msg + '\r'
@@ -77,11 +92,13 @@ def log(msg):
 ##    log.write(time.ctime() + '\t' + msg + '\r')
 ##    log.close()
 
+
 def error(msg):
     print msg
-##    log = open(ERROR_FILE, 'a')
-##    log.write(time.ctime() + '\t' + msg + '\r')
-##    log.close()
+    log = open(ERROR_FILE, 'a')
+    log.write(time.ctime() + '\t' + msg + '\r')
+    log.close()
+
 
 def cleanBlank(arr):
     newArr = []
@@ -92,25 +109,26 @@ def cleanBlank(arr):
             newArr.append(i)
     return newArr
 
+
 def save():
     global temp
-    log('Save')
+    log('Save,' + str(len(temp)))
     for key in temp:
-        save1(key, temp[key])
+        saveOneStock(key, temp[key])
     temp = {}
 
-def save1(stockNo, content):
+def saveOneStock(stockNo, content):
     path = RESULT_PATH + os.sep + stockNo + '.txt'
     stockFile = open(path, 'a')
     stockFile.write(content)
     stockFile.close()
+
 
 def do(file):
     global temp
     global counter
     #stockNo:lines
     startTime = time.time()
-    file = DATA_SRC + '/' + os.path.basename(file)
     f = open(file, 'r')
     for line in f:
         if len(line) < 10:
@@ -128,13 +146,13 @@ def do(file):
                 content += line
                 temp[stockNo] = content
     endStatis = time.time()
-    if counter % 500 == 0:
+    if counter % 1000 == 0:
         save()
     endSave = time.time()
     counter += 1
-    endTime = time.time()
     msg =  str(counter) + '/' + str(len(files)) + ',' + file + ',' + str(endTime - startTime) + ',' + str(endStatis - startTime) + ',' + str(endSave - endStatis)
     log(msg)
+
 
 def load():
     print 'load ' + allFile
@@ -148,6 +166,7 @@ def load():
             # print os.path.basename(path).split('.')[0].split('_')[1]
     f.close()
     
+
 def loadSortedFiles():
     print 'load ' + SORTED_FILE_NAMES
     f = open(SORTED_FILE_NAMES, 'r')
@@ -159,30 +178,31 @@ def loadSortedFiles():
             filesName.append(int(os.path.basename(path).split('.')[0].split('_')[1]))
     f.close()
 
-def main(startPoint):
+
+def main():
     log('Start...')
+
     # get all file, save to files
-    # tree(DATA_SRC)
-    loadSortedFiles()
+    tree(DATA_SRC)
+    saveFileName(UNSORT_FILE_NAMES)
+
+    # 排序并存储到文件中
+    insertion_sort()
+    saveFileName(SORTED_FILE_NAMES)
+
     log('Total file number : ' + str(len(files)))
+
+    # 清除上次处理的结果文件
     if os.path.exists(RESULT_PATH):
         shutil.rmtree(RESULT_PATH)
     os.mkdir(RESULT_PATH)
-    i = 0
+
     for file in files:
-        i += 1
-        if (i < startPoint) or (i >= startPoint+10000):
-            continue
         do(file)
 
+
 if __name__ == '__main__':
-    startPoint = int(sys.argv[1])
-    RESULT_PATH = RESULT_PATH + '_' + str(startPoint)
-    print startPoint, RESULT_PATH
-    main(startPoint)
-    # load()
-    # insertion_sort()
-    # saveFileName()
+    main()
     
 
 
