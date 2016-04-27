b80415d20b1f1d94bc87d3f952f8ea39463048af
Shuai Kang <kangshuai@tigerbrokers.com>
2016-04-27 18:17:18 +0800

    temp

--- TimeMillis1.py
+++ TimeMillis1.py
@@ -3,4 +3,12 @@ import os
 
 if __name__ == '__main__':
     time_str = raw_input('Enter Date(yyyy-mm-dd hh:mm:ss):\n')
-    print repr(time.mktime(time.strptime(time_str, '%Y-%m-%d %H:%M:%S')) * 1000)
+    if time_str is None or time_str.strip() is '':
+        print '--now--'
+        print int(time.time() * 1000)
+        print '----'
+    else:
+        print os.linesep
+        print '--{}--'.format(time_str)
+        print repr(int(time.mktime(time.strptime(time_str, '%Y-%m-%d %H:%M:%S')) * 1000))
+        print '----'
